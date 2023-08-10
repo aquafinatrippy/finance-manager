98377b37e05021d74bb7e56f912e2e2a1df2258c
aquafinatrippy <tanel.tekko@gmail.com>
2023-08-10 12:10:19 +0300

    Little clean up and backend endpoints tested n workin

--- Server/index.ts
+++ Server/index.ts
@@ -9,7 +9,8 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT;
 
-app.use("api/user", userRoutes);
+app.use(express.json());
+app.use("/api/user", userRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
