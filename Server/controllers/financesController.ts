1972eaa9349906c493ec3d292ab6135a370664dc
aquafinatrippy <tanel.tekko@gmail.com>
2023-08-14 13:21:37 +0300

    created update and delete for finance

--- Server/controllers/financesController.ts
+++ Server/controllers/financesController.ts
@@ -24,3 +24,50 @@ export const createFinance = async (req: Request, res: Response) => {
     res.status(400).json({ message: "Create finance error" });
   }
 };
+
+export const getFinanceData = async (req: Request, res: Response) => {
+  try {
+    const user = await User.findById(req.user.id);
+    if (!user) {
+      res.status(401).json({ message: "User not authenticated at creating" });
+    }
+    const resData = await Finance.find({ created_by: user?._id });
+    res.status(200).json(resData);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ message: "Create finance error" });
+  }
+};
+
+export const deleteFinance = async (req: Request, res: Response) => {
+  try {
+    const user = await User.findById(req.user.id);
+    if (!user) {
+      res.status(401).json({ message: "User not authenticated at creating" });
+    }
+    await Finance.findByIdAndRemove(req.params.id);
+    res.status(200).json({ message: "finance has been removed" });
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ message: "Delete finance error" });
+  }
+};
+
+export const updateFinance = async (req: Request, res: Response) => {
+  try {
+    const user = await User.findById(req.user.id);
+    if (!user) {
+      res.status(401).json({ message: "User not authenticated at creating" });
+    }
+    const { title, description, total_amount } = req.body;
+    const updatedFincance = await Finance.findByIdAndUpdate({
+      title,
+      description,
+      total_amount,
+    });
+    res.status(200).json(updatedFincance);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ message: "Update finance error" });
+  }
+};
--- Server/routes/financeRoute.ts
+++ Server/routes/financeRoute.ts
@@ -1,7 +1,12 @@
 import express from "express";
-import { createFinance } from "../controllers/financesController";
+import {
+  createFinance,
+  getFinanceData,
+} from "../controllers/financesController";
 import { authCheck } from "../middleware/authMiddleware";
 
 export const financeRoutes = express.Router();
 
+financeRoutes.route("/").get(authCheck, getFinanceData);
 financeRoutes.route("/create").post(authCheck, createFinance);
+financeRoutes.route("/:id").delete(authCheck, )
