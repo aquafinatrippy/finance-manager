194b3dca732cc9ed44c04c499c5a19fba66f280f
aquafinatrippy <tanel.tekko@gmail.com>
2023-08-14 14:22:05 +0300

    small ui improvments

--- Client/src/components/FinancialOutput.tsx
+++ Client/src/components/FinancialOutput.tsx
@@ -2,8 +2,8 @@ import { Button } from "@/components/ui/button";
 
 export const FinancialOutput = () => {
   return (
-    <div className="space-y-8">
-      <div className="group relative flex py-4 border-white border-2 rounded-lg items-center mt-4 hover:bg-gray-600 transition duration-300">
+    <div className="space-y-8 h-24">
+      <div className="group h-full relative flex py-4 border-white border-2 rounded-lg items-center mt-4 hover:bg-gray-600 transition duration-300">
         <div className="ml-4 space-y-1">
           <p className="text-sm text-white font-medium leading-none">
             Olivia Martin
@@ -16,13 +16,13 @@ export const FinancialOutput = () => {
 
         <Button
           height="20px"
-          className="absolute h-8 w-16 right-1/2 bottom-0 py-2 px-4 bg-blue-500 text-white opacity-0 group-hover:opacity-100 transition duration-300"
+          className="absolute h-8 w-16 right-1/2 bottom-2 py-2 px-4  text-white opacity-0 group-hover:opacity-100 transition duration-300"
         >
           Edit
         </Button>
         <Button
           variant="destructive"
-          className="absolute h-8 w-16 right-1/2 top-0 py-2 px-4 bg-red-500 text-white opacity-0 group-hover:opacity-100 transition duration-300"
+          className="absolute h-8 w-16 right-1/2 top-2 py-2 px-4 bg-red-500 text-white opacity-0 group-hover:opacity-100 transition duration-300"
         >
           Delete
         </Button>
